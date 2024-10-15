089a78109ab68bddf6a18f323a7cfced902ff7c5
francisco-ortin <ortin@uniovi.es>
2024-10-15 10:17:33 +0200

    Added object_detection.ipynb.

--- deep-learning/cnn/utils.py
+++ deep-learning/cnn/utils.py
@@ -5,8 +5,13 @@ import matplotlib.pyplot as plt
 import tensorflow_datasets as tfds
 
 
-def read_image_tfds(image, label):
-    """Places a 28x28 image randomly in 75x75 image, writing a bounding box around it"""
+def _read_image_tfds(image: tf.Tensor, label: tf.Tensor) -> (tf.Tensor, (tf.Tensor, tf.Tensor)):
+    """
+    Places a 28x28 image randomly in 75x75 image, writing a bounding box around it
+    :param image: the image
+    :param label: the label
+    :return: the image and the label with the bounding box
+    """
     xmin = tf.random.uniform((), 0, 48, dtype=tf.int32)
     ymin = tf.random.uniform((), 0, 48, dtype=tf.int32)
     image = tf.reshape(image, (28, 28, 1,))
@@ -21,11 +26,17 @@ def read_image_tfds(image, label):
     return image, (tf.one_hot(label, 10), [xmin, ymin, xmax, ymax])
 
 
-def get_train_val_dataset(batch_size: int, val_split:float=0.15):
-    """Downloads the training MNIST dataset from tensorflow datasets"""
+def get_train_val_dataset(batch_size: int, val_split: float = 0.15) -> (tf.data.Dataset, tf.data.Dataset):
+    """
+    Downloads the training MNIST dataset from tensorflow datasets
+    and splits it into training and validation datasets
+    :param batch_size: the batch size
+    :param val_split: the validation split
+    :return: the training and validation datasets
+    """
     dataset, ds_info = tfds.load('mnist', split='train', as_supervised=True, try_gcs=True, with_info=True)
     num_examples = ds_info.splits['train'].num_examples
-    dataset = dataset.map(read_image_tfds, num_parallel_calls=16)
+    dataset = dataset.map(_read_image_tfds, num_parallel_calls=16)
     dataset = dataset.shuffle(5000, reshuffle_each_iteration=True)
     dataset = dataset.repeat()
     train_dataset = dataset.take(train_size := int((1-val_split) * num_examples))
@@ -37,17 +48,29 @@ def get_train_val_dataset(batch_size: int, val_split:float=0.15):
     return train_dataset, val_dataset
 
 
-def get_test_dataset():
-    """Downloads the test MNIST dataset from tensorflow datasets"""
+def get_test_dataset() -> tf.data.Dataset:
+    """
+    Downloads the test MNIST dataset from tensorflow datasets
+    :return: the test dataset
+    """
     dataset = tfds.load("mnist", split="test", as_supervised=True, try_gcs=True)
-    dataset = dataset.map(read_image_tfds, num_parallel_calls=16)
+    dataset = dataset.map(_read_image_tfds, num_parallel_calls=16)
     dataset = dataset.batch(10000, drop_remainder=True)  # 10000 items in eval dataset, all in one batch
     dataset = dataset.repeat()  # Mandatory for Keras for now
     return dataset
 
 
-def dataset_to_numpy_util(train_ds, val_ds, test_ds, N):
-    """Converts the three raw tf datasets into np.arrays"""
+def dataset_to_numpy_util(train_ds: tf.data.Dataset, val_ds: tf.data.Dataset, test_ds: tf.data.Dataset, N: int) -> \
+        (np.array, np.array, np.array, np.array, np.array, np.array, np.array, np.array, np.array):
+    """
+    Converts the three raw tf datasets into NumPy's arrays
+    :param train_ds: the training dataset
+    :param val_ds: the validation dataset
+    :param test_ds: the test dataset
+    :param N: the number of elements to convert
+    :return: the numpy arrays (train_digits, train_labels, train_bboxes, val_digits, val_labels, val_bboxes,
+    test_digits, test_labels, test_bboxes)
+    """
     batch_train_ds = train_ds.unbatch().batch(N)
     if tf.executing_eagerly():
         for train_digits, (train_labels, train_bboxes) in batch_train_ds:
@@ -73,30 +96,59 @@ def dataset_to_numpy_util(train_ds, val_ds, test_ds, N):
             test_digits, test_labels, test_bboxes)
 
 
-def draw_bounding_boxes_on_image_array(image, boxes, color=[], thickness=1, display_str_list=()):
-    """Draws the bounding box in an image array"""
+def _draw_bounding_boxes_on_image_array(image: np.array, boxes: np.array, color=[], thickness=1, display_str_list=())\
+        -> np.array:
+    """
+    Draws the bounding box in an image array
+    :param image: the image
+    :param boxes: the boxes
+    :param color: the color
+    :param thickness: the thickness
+    :param display_str_list: the display string list
+    :return: the image array with the bounding boxes
+    """
     image_pil = PIL.Image.fromarray(image)
     rgbimg = PIL.Image.new('RGBA', image_pil.size)
     rgbimg.paste(image_pil)
-    draw_bounding_boxes_on_image(rgbimg, boxes, color, thickness, display_str_list)
+    _draw_bounding_boxes_on_image(rgbimg, boxes, color, thickness, display_str_list)
     return np.array(rgbimg)
 
 
-def draw_bounding_boxes_on_image(image, boxes, color=[], thickness=1, display_str_list=()):
-    """Draws bounding boxes in an image"""
+def _draw_bounding_boxes_on_image(image: PIL.Image, boxes: np.array, color=[], thickness=1, display_str_list=()) \
+        -> None:
+    """
+    Draws bounding boxes in an image
+    :param image: the image
+    :param boxes: the boxes
+    :param color: the color
+    :param thickness: the thickness
+    :param display_str_list: the display string list
+    """
     boxes_shape = boxes.shape
     if not boxes_shape:
         return
     if len(boxes_shape) != 2 or boxes_shape[1] != 4:
         raise ValueError('Input must be of size [N, 4]')
     for i in range(boxes_shape[0]):
-        draw_bounding_box_on_image(image, boxes[i, 1], boxes[i, 0], boxes[i, 3], boxes[i, 2], color[i], thickness,
-                                   display_str_list[i])
+        _draw_bounding_box_on_image(image, boxes[i, 1], boxes[i, 0], boxes[i, 3], boxes[i, 2], color[i], thickness,
+                                    display_str_list[i])
 
 
-def draw_bounding_box_on_image(image, ymin, xmin, ymax, xmax, color='red', thickness=1,
-                               display_str_list=None, use_normalized_coordinates=True):
-    """Draws a bounding box in an image"""
+def _draw_bounding_box_on_image(image: PIL.Image, ymin: float, xmin: float, ymax: float, xmax: float,
+                                color: str = 'red', thickness: int = 1, display_str_list: list[str] = None,
+                                use_normalized_coordinates:bool = True) -> None:
+    """
+    Draws a bounding box in an image
+    :param image: the image
+    :param ymin: the minimum y
+    :param xmin: the minimum x
+    :param ymax: the maximum y
+    :param xmax: the maximum x
+    :param color: the color
+    :param thickness: the thickness
+    :param display_str_list: the display string list
+    :param use_normalized_coordinates: whether to use normalized
+    """
     draw = PIL.ImageDraw.Draw(image)
     im_width, im_height = image.size
     if use_normalized_coordinates:
@@ -106,8 +158,17 @@ def draw_bounding_box_on_image(image, ymin, xmin, ymax, xmax, color='red', thick
     draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=thickness, fill=color)
 
 
-def display_digits_with_boxes(digits, predictions, labels, pred_bboxes, bboxes, title):
-    """Shows predictions and bounding boxes"""
+def display_digits_with_boxes(digits: np.array, predictions: np.array, labels: np.array, pred_bboxes: np.array,
+                              bboxes: np.array, title: str) -> None:
+    """
+    Shows predictions and bounding boxes
+    :param digits: the digits
+    :param predictions: the predictions
+    :param labels: the labels
+    :param pred_bboxes: the predicted bounding boxes
+    :param bboxes: the bounding boxes
+    :param title: the title of the figure
+    """
     n = 10
     indexes = np.random.choice(len(predictions), size=n)
     n_digits = digits[indexes]
@@ -130,7 +191,7 @@ def display_digits_with_boxes(digits, predictions, labels, pred_bboxes, bboxes, 
             bboxes_to_plot.append(n_pred_bboxes[i])
         if len(bboxes) > i:
             bboxes_to_plot.append(n_bboxes[i])
-        img_to_draw = draw_bounding_boxes_on_image_array(image=n_digits[i], boxes=np.asarray(bboxes_to_plot), color=['red', 'green'], display_str_list=["true", "pred"])
+        img_to_draw = _draw_bounding_boxes_on_image_array(image=n_digits[i], boxes=np.asarray(bboxes_to_plot), color=['red', 'green'], display_str_list=["true", "pred"])
         plt.xlabel(n_predictions[i])
         plt.xticks([])
         plt.yticks([])
