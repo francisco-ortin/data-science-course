20189306caea54cf3fd9a7cf5a6f7b8aac5f53be
francisco-ortin <ortin@uniovi.es>
2024-10-28 18:48:13 +0100

    First version of simple_rnn.ipynb (all the code in one cell).

--- deep-learning/rnn/simple_rnn_backup.py
+++ deep-learning/rnn/simple_rnn_backup.py
@@ -1,4 +1,5 @@
 # The ridership data comes from Chicago's Transit Authority, and was downloaded from the Chicago's Data Portal at https://data.cityofchicago.org/Transportation/CTA-Ridership-Daily-Boarding-Totals/6iiy-9s97/about_data
+import numpy as np
 # The dataset contains the  daily number of bus and train passengers from 2001 to 2021.
 
 # The dataset contains the following columns:
@@ -13,7 +14,6 @@ import matplotlib.pyplot as plt
 from keras import Model
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
-import numpy as np
 
 ## LOAD THE DATA
 
@@ -65,11 +65,13 @@ def create_variable_time_series_dataset(dataset_p: pd.DataFrame, length_from_p: 
     :return: (X, y) where X is the time series and y is the target variable
     """
     X, y = [], []
-    for i in range(0, len(dataset_p)):
+    i = 0
+    while i < len(dataset_p):
         series_length = np.random.randint(length_from_p, length_to_p + 1)
         if i + series_length < len(dataset_p):
             X.append(np.array(dataset_p.iloc[i:i + series_length][['rail', 'day_type_A', 'day_type_U', 'day_type_W']].values))
             y.append(dataset_p.iloc[i + series_length]['rail'])
+            i += series_length
         else:
             break
     return X, y
@@ -79,10 +81,43 @@ length_from, length_to = 40, 60
 X_train, y_train = create_variable_time_series_dataset(train_ds, length_from, length_to)
 X_val, y_val = create_variable_time_series_dataset(val_ds, length_from, length_to)
 X_test, y_test = create_variable_time_series_dataset(test_ds, length_from, length_to)
+print(f"Length of X: {len(X_train)}")
+print(f"Length of y: {len(y_train)}")
+print("First few elements of X:")
+print(X_train[:1])
+print("First few elements of y:")
+print(y_train[:5])
 
+
+# Sample variable-length sequences for X and corresponding scalar targets for y
+"""X = [
+    np.array([[0.1], [0.2], [0.3]]),  # Sequence of 3 timesteps
+    np.array([[0.5], [0.6]]),  # Sequence of 2 timesteps
+    np.array([[0.4], [0.8], [0.9], [1.0]]),  # Sequence of 4 timesteps
+    np.array([[0.1]]),  # Sequence of 1 timestep
+    np.array([[0.7], [0.2], [0.9]])  # Sequence of 3 timesteps
+]
+
+y = [
+    0.3,  # Target for the first sequence
+    0.5,  # Target for the second sequence
+    0.7,  # Target for the third sequence
+    0.1,  # Target for the fourth sequence
+    0.4  # Target for the fifth sequence
+]
+"""
 
 # Select sequences of random length between 30 and 60 days using tf.keras.utils.timeseries_dataset_from_array
 
+def fit_and_evaluate(model_p: Model, train_set_p: pd.DataFrame, val_ds_p: pd.DataFrame, learning_rate: float, epochs: int=500) -> float:
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(
+        monitor="val_mae", patience=50, restore_best_weights=True)
+    opt = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9)
+    model_p.compile(loss=tf.keras.losses.Huber(), optimizer=opt, metrics=["mae"])
+    history = model_p.fit(train_set_p, validation_data=val_ds_p, epochs=epochs,
+                          callbacks=[early_stopping_cb], batch_size=1)  # batch_size=1 for RNNs to allow for variable length sequences
+    valid_loss, valid_mae = model_p.evaluate(val_ds_p)
+    return valid_mae * 1e6
 
 def X_y_to_tensor_slices(X_p: list, y_p: list) -> tf.data.Dataset:
     # Convert to TensorFlow Dataset
@@ -111,102 +146,24 @@ val_set_tf = val_set_tf.batch(1).prefetch(1)
 test_set_tf = test_set_tf.batch(1).prefetch(1)
 
 # Define the model
-simple_model = tf.keras.Sequential([
+model = tf.keras.Sequential([
     tf.keras.layers.SimpleRNN(1, input_shape=[None, 4])  # None = variable length sequence, 4 = number of features in each timestep
 ])
-print(simple_model.summary())
 
-def train_model(model: Model, train_set_p: tf.data.Dataset, val_set_p: tf.data.Dataset, learning_rate: float, epochs: int) \
-        -> tf.keras.callbacks.History:
-    # Compile the model
-    optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9)
-    # huber loss is less is useful for time series data
-    model.compile(optimizer=optimizer, loss=tf.keras.losses.Huber(), metrics=['mae', 'mape'])
-    early_stopping_cb = tf.keras.callbacks.EarlyStopping(
-        monitor="val_mae", patience=5, restore_best_weights=True)
-    history = model.fit(train_set_p, validation_data=val_set_p, epochs=epochs,
-                        batch_size=1, callbacks=[early_stopping_cb])
-    return history
+# Compile the model
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.02, momentum=0.9)
+model.compile(optimizer=optimizer, loss=tf.keras.losses.Huber(), metrics=['mae', 'mape'])
 
+early_stopping_cb = tf.keras.callbacks.EarlyStopping(
+    monitor="val_mae", patience=5, restore_best_weights=True)
+# Train the model without specifying batch_size (since dataset is already batched)
+history = model.fit(train_set_tf, validation_data=val_set_tf, epochs=500,
+                    batch_size=1, callbacks=[early_stopping_cb])
 
-epochs = 1
-train_model(simple_model, train_set_tf, val_set_tf, learning_rate=0.01, epochs=epochs)
-test_loss, test_mae, test_mape = simple_model.evaluate(test_set_tf)
-# The test MAE is scaled, so we need to inverse the scaling to get the actual value.
-# Since the output is a 2d array (bath_size, y features), we need to select the first element of the first element of the array.
+test_loss, test_mae, test_mape = model.evaluate(train_set_tf)
 print(f"Test MAE: {scaler.inverse_transform([[test_mae]])[0][0]:.0f}.")
 print(f"Test MAPE: {test_mape:.2f}.")
 
-
-def plot_predictions(y_test_p: np.array, y_pred_p: np.array, n_instances: int):
-    """
-    Plot the first 100 instances of y_pred vs y_test
-    :param y_test_p: the true values
-    :param y_pred_p: the predicted values
-    """
-    plt.figure(figsize=(12, 3.5))
-    plt.plot(y_test_p[:n_instances], label="True values")
-    plt.plot(y_pred_p[:n_instances], label="Predicted values")
-    plt.legend()
-    plt.show()
-
-
-n_instances_to_plot = 100
-y_pred = simple_model.predict(test_set_tf)
-plot_predictions(y_test, y_pred, n_instances_to_plot)
-
-
-## MULTIPLE SIMPLE RECURRENT NEURONS
-
-# The model is not very accurate, but it is a simple model. We can try to improve the model by adding a layer of simple RNNs.
-# Instead of one simple recurrent neuron, we can add 32 simple recurrent neurons to the model.
-# Since the output has to be a scalar, we need to add a Dense layer with one neuron at the end of the model.
-
-multi_model = tf.keras.Sequential([
-    tf.keras.layers.SimpleRNN(32, input_shape=[None, 4]),
-    tf.keras.layers.Dense(1)  # no activation function by default (regression model)
-])
-print(multi_model.summary())
-
-train_model(multi_model, train_set_tf, val_set_tf, learning_rate=0.01, epochs=epochs)
-test_loss, test_mae, test_mape = multi_model.evaluate(test_set_tf)
-print(f"Test MAE: {scaler.inverse_transform([[test_mae]])[0][0]:.0f}.")
-print(f"Test MAPE: {test_mape:.2f}.")
-
-y_pred = multi_model.predict(test_set_tf)
-plot_predictions(y_test, y_pred, n_instances_to_plot)
-
-
-## DEEP RNN
-
-# We can also try to improve the model by adding more layers to the model.
-# We can add three layers of simple RNNs with 32 neurons each.
-# Each layer will return the output of each time step, so they can be stacked.
-# To that aim we set return_sequences=True.
-# We can also add a Dense layer with one neuron at the end of the model.
-
-
-deep_model = tf.keras.Sequential([
-    # return_sequences=True is used to return the output of each time step, not just the last one
-    # in this way, the next layer can process the output of each time step (they can be stacked)
-    tf.keras.layers.SimpleRNN(32, input_shape=[None, 4], return_sequences=True),
-    tf.keras.layers.SimpleRNN(32, return_sequences=True),
-    tf.keras.layers.SimpleRNN(32, input_shape=[None, 4]),
-    tf.keras.layers.Dense(1)  # no activation function by default (regression model)
-])
-print(deep_model.summary())
-
-train_model(deep_model, train_set_tf, val_set_tf, learning_rate=0.01, epochs=epochs)
-test_loss, test_mae, test_mape = deep_model.evaluate(test_set_tf)
-print(f"Test MAE: {scaler.inverse_transform([[test_mae]])[0][0]:.0f}.")
-print(f"Test MAPE: {test_mape:.2f}.")
-
-y_pred = deep_model.predict(test_set_tf)
-plot_predictions(y_test, y_pred, n_instances_to_plot)
-
-
-## QUESTIONS:
-# 1. Are model preformed being improved by adding more complexity/parameters?
-# 2. Why?
-# 3. Do you think that will always be the case?
+val_loss, val_mae, val_mape = model.evaluate(val_set_tf)
+print(f"Val MAPE: {val_mape:.2f}.")
 
